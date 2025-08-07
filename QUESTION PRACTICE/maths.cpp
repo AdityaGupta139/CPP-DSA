8a1f705e95de0364d99bd4f5f77e6bd4106430e0
ADITYA GUPTA <167164745+AdityaGupta139@users.noreply.github.com>
2025-08-07 21:57:41 +0530

    .

--- QUESTION PRACTICE/maths.cpp
+++ QUESTION PRACTICE/maths.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-void maths(int n)
+void maths(int n1,int n2)
 {
     // 1.digits
     // while(n>0){
@@ -86,14 +86,38 @@ void maths(int n)
 //     }
 
 
-// prime number
+// gcd 
+
+// for(int i=(n,m);i>=1;i--){
+// if(n%i==0 && m%i==0){
+//     cout<<i;
+//     break;
+// }
+// }
     
+while(n1>0 && n2>0){
+    if(n1>n2){
+        n1=n1%n2;
+    }
+    else{
+        n2=n2%n1;
+    }
+    
+}
+if(n1==0){
+cout<<n2;
+}
+else{
+    cout<<n1;
+}
 }
 
 int main()
 {
-    int n;
+    int n1,n2;
     cout << "Enter number:";
-    cin >> n;
-    maths(n);
+    cin >>n1;
+    cout<<"Enter 2nd number";
+    cin>>n2;
+    maths(n1,n2);
 }
