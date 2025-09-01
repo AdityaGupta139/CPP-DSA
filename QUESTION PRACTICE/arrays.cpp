eed508c7f9a13c97a4a89eb0cc2ed66ed44cbf5c
ADITYA GUPTA <gaditya2005@gmail.com>
2025-09-01 15:56:36 +0530

    Update arrays.cpp

--- QUESTION PRACTICE/arrays.cpp
+++ QUESTION PRACTICE/arrays.cpp
@@ -248,8 +248,24 @@ int singleNumber(vector<int> &nums)
     }
     return XOR;
 }
-// jftf
 
+    vector<int> twoSum(vector<int>& arr, int target) {
+        int n=arr.size();
+        vector<int>ans;
+        map<int,int>mpp;
+        for(int i=0;i<n;i++){
+            int a=arr[i];
+            int more=target-a;
+            if(mpp.find(more)!=mpp.end()){
+                ans.push_back(mpp[more]);
+                ans.push_back(i);
+            }
+            mpp[a]=i;
+        }
+        
+       return ans;
+        
+    }
 
 int main()
 {
