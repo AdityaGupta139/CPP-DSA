9f010c55a1a3937c98c63de5c341080ff539e3b4
ADITYA GUPTA <gaditya2005@gmail.com>
2025-09-14 21:46:43 +0530

    Update arrays.cpp

--- QUESTION PRACTICE/arrays.cpp
+++ QUESTION PRACTICE/arrays.cpp
@@ -485,7 +485,20 @@ if(leader==true){
     }
     return cnt;
 }
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int>mpp;
+        mpp[0]=1;
+        int presum=0;int cnt=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            presum+=nums[i];
+            int remove=presum-k;
+            cnt+=mpp[remove];
+            mpp[presum]+=1;
+        }
 
+        return cnt;
+    }
 int main()
 {
     int n;
