c7ef01a1ef866a87f38448c43401cdf77fa767fb
ADITYA GUPTA <167164745+AdityaGupta139@users.noreply.github.com>
2025-10-25 00:58:01 +0530

    Update Merge_Sorted_array.cpp

--- ARRAYS/HARD/Merge_Sorted_array.cpp
+++ ARRAYS/HARD/Merge_Sorted_array.cpp
@@ -1,2 +1,20 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int left=m-1;
+        int right=n-1;
+        int p=m+n-1;
+        while(right>=0){
+            if(left>=0 && nums1[left]>nums2[right] ){
+                nums1[p]=nums1[left];
+                left--;
 
-    
+            }
+            else{
+                nums1[p]=nums2[right];
+                right--;
+            }
+            p--;
+        }
+    }
+};
