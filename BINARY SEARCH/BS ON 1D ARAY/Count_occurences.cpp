a1f4742d0abfa767aaa6dea36ed095297dc3c8fa
ADITYA GUPTA <167164745+AdityaGupta139@users.noreply.github.com>
2025-10-27 11:53:24 +0530

    .

---
+++ BINARY SEARCH/BS ON 1D ARAY/Count_occurences.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+int lowerBound(vector<int> arr, int n, int x) {
+	int low=0,high=n-1;
+	int ans=n;
+	while(low<=high){
+		int mid=(low+high)/2;
+		if(arr[mid]>=x){
+			ans=mid;
+			high=mid-1;
+
+		}
+		else{
+			low=mid+1;
+		}
+	}
+	return ans;
+}
+int upperBound(vector<int> &arr, int n, int x){
+		int low=0,high=n-1;
+	int ans=n;
+	while(low<=high){
+		int mid=(low+high)/2;
+		if(arr[mid]>x){
+			ans=mid;
+			high=mid-1;
+
+		}
+		else{
+			low=mid+1;
+		}
+	}
+	return ans;
+}
+vector<int> searchRange(vector<int>& nums, int target) {
+        int n=nums.size();
+        int first=lowerBound(nums,n,target);
+        if(first==n ||nums[first]!=target ){
+            return {-1,-1};
+        }
+        int last=upperBound(nums,n,target)-1;
+        return {first,last};
+
+    }
+
+int count(vector<int>& arr, int n, int x) {
+	vector<int>ans=searchRange(arr,x);
+	if(ans[0]==-1)return 0;
+	return ans[1] - ans[0] + 1;
+}
---
+++ BINARY SEARCH/BS ON 1D ARAY/First_last_occurrenc.cpp
@@ -0,0 +1,42 @@
+int lowerBound(vector<int> arr, int n, int x) {
+	int low=0,high=n-1;
+	int ans=n;
+	while(low<=high){
+		int mid=(low+high)/2;
+		if(arr[mid]>=x){
+			ans=mid;
+			high=mid-1;
+
+		}
+		else{
+			low=mid+1;
+		}
+	}
+	return ans;
+}
+int upperBound(vector<int> &arr, int n, int x){
+		int low=0,high=n-1;
+	int ans=n;
+	while(low<=high){
+		int mid=(low+high)/2;
+		if(arr[mid]>x){
+			ans=mid;
+			high=mid-1;
+
+		}
+		else{
+			low=mid+1;
+		}
+	}
+	return ans;
+}
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n=nums.size();
+        int first=lowerBound(nums,n,target);
+        if(first==n ||nums[first]!=target ){
+            return {-1,-1};
+        }
+        int last=upperBound(nums,n,target)-1;
+        return {first,last};
+
+    }
